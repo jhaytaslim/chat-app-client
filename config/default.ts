682096b24f7c160ac6539786adcc50d348738bdd
apple <jhaytaslim@gmail.com>
2022-02-21 22:56:58 +0100

    initial commit

---
+++ config/default.ts
@@ -0,0 +1,2 @@
+export const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:4000";
+export const BASE_URL = process.env.BASE_URL || "http://localhost:4000";
---
+++ containers/Rooms.tsx
@@ -0,0 +1,70 @@
+import { useRef } from "react";
+import EVENTS from "../config/events";
+import { useSockets } from "../context/socket.context";
+import styles from "../styles/Room.module.css";
+import axios from 'axios';
+import { BASE_URL } from "../config/default";
+
+function RoomsContainer() {
+  const { socket, roomId, rooms } = useSockets();
+  const newRoomRef = useRef(null);
+
+  async function handleCreateRoom() {
+    //get the room name
+    const roomName = newRoomRef.current.value || "";
+
+    if (!String(roomName).trim()) return;
+
+    // // emit room created event
+    // socket.emit(EVENTS.CLIENT.CREATE_ROOM, { roomName });
+
+    const axiosReq = {
+      method: 'POST',
+      url: `${BASE_URL}/talk`,
+      headers: { 'Content-Type': 'application/json' },
+      data: {name: roomName}
+    } 
+    console.log("axiosReq: ",axiosReq)
+    const { status, data, message }  = await axios(axiosReq)
+
+    console.log("info: ", data,status,message)
+
+    // set room name input to empty string
+    newRoomRef.current.value = "";
+  }
+
+  function handleJoinRoom(key) {
+    if (key === roomId) return;
+
+    socket.emit(EVENTS.CLIENT.JOIN_ROOM, key);
+  }
+
+  return (
+    <nav className={styles.wrapper}>
+      <div className={styles.createRoomWrapper}>
+        <input ref={newRoomRef} placeholder="Room name" />
+        <button className="cta" onClick={handleCreateRoom}>
+          CREATE ROOM
+        </button>
+      </div>
+
+      <ul className={styles.roomList}>
+        {Object.keys(rooms).map((key) => {
+          return (
+            <div key={key}>
+              <button
+                disabled={key === roomId}
+                title={`Join ${rooms[key].name}`}
+                onClick={() => handleJoinRoom(key)}
+              >
+                {rooms[key].name}
+              </button>
+            </div>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
+
+export default RoomsContainer;
